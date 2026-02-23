945824426337e6dbe4d0b598a0637238dc78c76b
jota0922 <kenta.uejo@gmail.com>
2026-02-23 14:50:16 +0900

    Add Dockerfile and port settings

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
