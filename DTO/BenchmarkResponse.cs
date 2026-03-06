146e87639a0bafaf38d96863e33053a519ec8e35
jota0922 <kenta.uejo@gmail.com>
2026-03-06 19:59:17 +0900

    add benchmark page

---
+++ DTO/BenchmarkResponse.cs
@@ -0,0 +1,25 @@
+public class BenchmarkResponse
+{
+    [JsonPropertyName("benchmark_progress")]
+    public double BenchmarkProgress { get; set; }
+    [JsonPropertyName("overall_rank")]
+    public int OverallRank { get; set; }
+    [JsonPropertyName("categories")]
+    public Dictionary<string, Category> Categories { get; set; } = new();
+}
+
+public class Category
+{
+    [JsonPropertyName("benchmark_progress")]
+    public double BenchmarkProgress { get; set; }
+    [JsonPropertyName("scenarios")]
+    public Dictionary<string, ScenarioScore> Scenarios { get; set; } = new();
+}
+
+public class ScenarioScore
+{
+    [JsonPropertyName("score")]
+    public double Score { get; set; }
+    [JsonPropertyName("leaderboard_rank")]
+    public int? LeaderboardRank { get; set; }
+}
